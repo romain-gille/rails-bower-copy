93d4658dc3bd85041a8f00a616c0775e604bef8a
romain gille <gille.romain@yahoo.fr>
2022-03-16 20:57:55 +0100

    filter and sort functions added

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -3,14 +3,28 @@ require 'open-uri'
 
 class BaseController < ApplicationController
   def home
-    @packages = %w[Bootstrap FontAwesome]
-    @packsinfos = getinfo.first(5)
+    @page = if params[:page].present? && params[:page].to_i.positive?
+              params[:page].to_i
+            else
+              1
+            end
+
+    @sort = if params[:sort].present?
+              params[:sort]
+            else
+              'name'
+            end
+    @packsinfos = if params[:query].present?
+                    getinfo(params[:query]).sort_by! { |e| e[@sort].downcase }[((@page - 1) * 5)...(@page * 5)]
+                  else
+                    getinfo('').sort_by! { |e| e[@sort].downcase }[((@page - 1) * 5)...(@page * 5)]
+                  end
   end
 
   private
 
-  def getinfo
-    url = 'https://libraries.io/api/bower-search?q='
+  def getinfo(search)
+    url = "https://libraries.io/api/bower-search?q=#{search}"
     packs_serialized = URI.open(url).read
     JSON.parse(packs_serialized)
   end
