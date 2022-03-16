ec84491a3a5d76cb7e654e18fe87254b1d1451c5
romain gille <gille.romain@yahoo.fr>
2022-03-16 17:34:29 +0100

    bower json fetch

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,5 +1,17 @@
+require 'json'
+require 'open-uri'
+
 class BaseController < ApplicationController
   def home
     @packages = %w[Bootstrap FontAwesome]
+    @packsinfos = getinfo.first(5)
+  end
+
+  private
+
+  def getinfo
+    url = 'https://libraries.io/api/bower-search?q='
+    packs_serialized = URI.open(url).read
+    JSON.parse(packs_serialized)
   end
 end
